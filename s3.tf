e0cb11ba89e1f55afe8a47265dd7d2fd4183ce68
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-19 19:03:42 +0530

    s3

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
-  bucket = "pipeline-artifacts-terraformaws"
+  bucket = "pipeline-artifacts"
  
 
   acl    = "private"
--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "mybucketforconsole"
+        bucket = "myterraformbucketinaws"
         encrypt = true
         key = "terraform.tfstate"
         region = "us-east-1"
